c5d721f478b82a0b94edf009b50720802c458329
drumath2237 <2237k.drumath@gmail.com>
2023-12-24 01:35:22 +0900

    :sparkles: babylonjsのシーンを再生デイタ

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
-import './style.css'
+import "./style.scss";
 
-const main = ()=>{}
+import { Engine, MeshBuilder, Scene, Vector3 } from "@babylonjs/core";
+
+const main = () => {
+  const renderCanvas = document.getElementById(
+    "renderCanvas"
+  ) as HTMLCanvasElement;
+  if (!renderCanvas) {
+    return;
+  }
+
+  const engine = new Engine(renderCanvas, true);
+  const scene = new Scene(engine);
+
+  scene.createDefaultCameraOrLight(true, true, true);
+
+  const box = MeshBuilder.CreateBox("box", { size: 0.1 });
+  box.position = new Vector3(0, 0.05, 0);
+
+  window.addEventListener("resize", () => engine.resize());
+  engine.runRenderLoop(() => scene.render());
+};
 
 main();

6061de25d34369c9af4101451190f7cb5e2e73af
drumath2237 <2237k.drumath@gmail.com>
2023-12-25 00:13:54 +0900

    :wrench: 型定義ファイルを出力するように設定を変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
+  plugins: [dts()],
   build: {
     lib: {
-      entry: "./lib/index.ts",
+      entry: resolve(__dirname, "lib/index.ts"),
       name: "luma-splatting-for-babylonjs",
       fileName: "index",
       formats: ["es", "umd"],
+    },
+    rollupOptions: {
+      external: ["@babylonjs/core"],
+      output: {
+        globals: {
+          "@babylonjs/core": "BABYLON",
+        },
+      },
     },
   },
 });

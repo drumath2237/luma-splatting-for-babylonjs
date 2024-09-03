dd4e6f5908d35c81742ddfe68b492f6ebcf17e65
drumath <2237k.drumath@gmail.com>
2024-09-04 03:34:43 +0900

    :recycle: fix formatter issue

--- lib/api/LumaAPIClient.ts
+++ lib/api/LumaAPIClient.ts
@@ -1,31 +1,31 @@
 import type { LumaSplattingResponseType, SplatDataUrlsType } from "./types";
 
 export const getSplatAndMetaDataUrlsAsync = async (
-	uuid: string,
+  uuid: string,
 ): Promise<SplatDataUrlsType | null> => {
-	const lumaaiBaseEndpoint: string =
-		"https://webapp.engineeringlumalabs.com/api/v3/captures";
-	const reqURL = `${lumaaiBaseEndpoint}/${uuid}/public`;
-	const lumaResponse = await fetch(reqURL, {
-		method: "GET",
-		headers: {
-			"Content-Type": "application/json",
-		},
-	}).then((res) => res.json() as Promise<LumaSplattingResponseType>);
+  const lumaaiBaseEndpoint: string =
+    "https://webapp.engineeringlumalabs.com/api/v3/captures";
+  const reqURL = `${lumaaiBaseEndpoint}/${uuid}/public`;
+  const lumaResponse = await fetch(reqURL, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json() as Promise<LumaSplattingResponseType>);
 
-	if (!lumaResponse.response?.artifacts) {
-		return null;
-	}
-	const artifacts = lumaResponse.response.artifacts;
-	const plyUrl = artifacts.find((a) => a.type === "gs_point_cloud");
-	const metaDataUrl = artifacts.find((a) => a.type === "gs_point_cloud_meta");
+  if (!lumaResponse.response?.artifacts) {
+    return null;
+  }
+  const artifacts = lumaResponse.response.artifacts;
+  const plyUrl = artifacts.find((a) => a.type === "gs_point_cloud");
+  const metaDataUrl = artifacts.find((a) => a.type === "gs_point_cloud_meta");
 
-	if (!plyUrl || !metaDataUrl) {
-		return null;
-	}
+  if (!plyUrl || !metaDataUrl) {
+    return null;
+  }
 
-	return {
-		metaFileUrl: metaDataUrl.url,
-		plyFilesUrl: plyUrl.url,
-	};
+  return {
+    metaFileUrl: metaDataUrl.url,
+    plyFilesUrl: plyUrl.url,
+  };
 };
